b79e02658077e0ce1dc051b4ff92d5a10a186d79
Evant33 <evanthompson438@gmail.com>
2020-10-15 10:23:38 -0700

    updated scripts

--- PycharmProjects/pythonProject/Regression.py
+++ PycharmProjects/pythonProject/Regression.py
@@ -4,25 +4,34 @@ import sklearn
 from sklearn import linear_model
 from sklearn.utils import shuffle
 
+
+# read in student grade data
 data = pd.read_csv("student-mat.csv", sep=";")
-
 data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
-
 predict = "G3"
 
-#print(data.head(50))
+# print(data.head(50))
 
+# convert to arrays
 X = np.array(data.drop([predict], 1))
 y = np.array(data[predict])
 
+# train test split
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
 
+# define model, calculate model and print accuracy
 linear = linear_model.LinearRegression()
-
 linear.fit(x_train, y_train)
 acc = linear.score(x_test, y_test)
 print(acc)
 
+# predict coefficients of linear model
 print("Co: \n", linear.coef_)
 print("Intercept: \n", linear.intercept_)
+
+# print data and predictions side by side for test set
+predictions = linear.predict(x_test)
+for x in range(len(predictions)):
+    print(predictions[x], x_test[x], y_test[x])
+
 
