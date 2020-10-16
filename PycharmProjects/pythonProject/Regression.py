e858f9e2d3d42cbfabb7f0c06a4a768573283696
Evant33 <evanthompson438@gmail.com>
2020-10-15 17:56:54 -0700

    new jupyter notebook

--- PycharmProjects/pythonProject/Regression.py
+++ PycharmProjects/pythonProject/Regression.py
@@ -2,7 +2,10 @@ import pandas as pd
 import numpy as np
 import sklearn
 from sklearn import linear_model
-from sklearn.utils import shuffle
+import matplotlib.pyplot as pyplot
+import pickle
+from matplotlib import style
+
 
 
 # read in student grade data
@@ -16,14 +19,33 @@ predict = "G3"
 X = np.array(data.drop([predict], 1))
 y = np.array(data[predict])
 
-# train test split
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
 
-# define model, calculate model and print accuracy
-linear = linear_model.LinearRegression()
-linear.fit(x_train, y_train)
-acc = linear.score(x_test, y_test)
-print(acc)
+
+
+
+'''
+best = 0 # best score is zero. if we score better than 0, save this as model
+for _ in range(30):
+    # train test split
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
+
+    # define model, calculate model and print accuracy
+    linear = linear_model.LinearRegression()
+    linear.fit(x_train, y_train)
+    acc = linear.score(x_test, y_test)
+    print(acc)
+
+    if acc > best:
+        best = acc
+        with open("model.pickle", "wb") as f:
+            pickle.dump(linear, f)'''
+
+
+pickle_in = open("model.pickle", "rb")
+linear = pickle.load(pickle_in)
+
+
 
 # predict coefficients of linear model
 print("Co: \n", linear.coef_)
@@ -33,5 +55,17 @@ print("Intercept: \n", linear.intercept_)
 predictions = linear.predict(x_test)
 for x in range(len(predictions)):
     print(predictions[x], x_test[x], y_test[x])
+
+
+
+p = "G1"
+style.use("ggplot")
+pyplot.scatter(data[p], data["G3"])
+pyplot.xlabel(p)
+pyplot.ylabel("Final Grade")
+pyplot.show()
+
+
+
 
 
